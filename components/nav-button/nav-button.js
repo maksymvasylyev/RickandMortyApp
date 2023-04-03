0b459c2bd81e1cf289fc548075a6e634520f436e
Henrike Stahlhut <henrike.st@icloud.com>
2023-04-03 15:22:21 +0200

    last touchup

--- components/nav-button/nav-button.js
+++
@@ -1,5 +0,0 @@
-import { prevButton } from "../../index.js";
-import { nextButton } from "../../index.js";
-import { navigation } from "../../index.js";
-import { pagination } from "../../index.js";
-import {page} from "../../index.js";
