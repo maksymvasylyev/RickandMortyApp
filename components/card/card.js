d65ccb71470716df511cf980a1eb910196fd8f63
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-04-03 10:23:58 +0200

    add new card

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,27 @@
-export function createCharacterCard() {}
+export function createCharacterCard() {
+  const newCard = document.createElement("li");
+  newCard.classList.add("card");
+  newCard.innerHTML = `
+    <div class="card__image-container">
+            <img
+              class="card__image"
+              src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+              alt="Rick Sanchez"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">Rick Sanchez</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">Alive</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description"></dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">51</dd>
+            </dl>
+          </div>
+    `;
+
+  return newCard;
+}
